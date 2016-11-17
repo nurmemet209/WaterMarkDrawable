c1d05c86e5b4b9e45b9867d7820dc2c8a61f232f
nurmemet <2335515050@qq.com>
2016-11-17 08:55:40 +0800

    secreenshot

--- library/src/main/java/com/cn/nurmemet/MarkDrawable.java
+++ library/src/main/java/com/cn/nurmemet/MarkDrawable.java
@@ -1,9 +1,7 @@
 package com.cn.nurmemet;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.ColorFilter;
-import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.RectF;
